49af42f2bc2e1638537c47583d28e7b2804ce20c
Pulkit Taneja <tanejapulkit@microsoft.com>
2024-11-17 17:28:56 +0530

    Firebase wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import './App.css'; // Importing the CSS file for styling
 
 function SpecsInput() {
+  const [customerName, setCustomerName] = useState("");
+  const [rightAddition, setRightAddition] = useState("");
+  const [leftAddition, setLeftAddition] = useState("");
   const [rightSph, setRightSph] = useState("");
   const [rightCyl, setRightCyl] = useState("");
   const [rightAxis, setRightAxis] = useState("");
@@ -16,12 +19,10 @@ function SpecsInput() {
 
   const handleShare = () => {
     const file = new File([imageBlob], "specs-power-image.png", { type: "image/png" });
-    const fileUrl = URL.createObjectURL(file);
 
     if (navigator.share) {
       navigator.share({
         files: [file],
-        url: fileUrl,
         title: "Specs Power Image",
         text: "Check out my specs power details",
       })
@@ -51,29 +52,63 @@ function SpecsInput() {
     }
   
     return formatted;
-  }  
+  }
 
+  const divideDescription = (description, maxLineLength) => {
+    let descriptionWords = description.split(" ")
+    let lines = []
+    let currentLine = ""
+    descriptionWords.forEach(word => {
+      if (currentLine.length + word.length > maxLineLength) {
+        lines.push(currentLine)
+        currentLine = ""
+      }
+      currentLine = currentLine + " " + word
+    })
+    lines.push(currentLine)
+    return lines
+  }
+  
   const generateImage = (data) => {
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
-
+    
     canvas.width = 800;
-    canvas.height = 600;
-
-    context.fillStyle = "#ffffff";
+    canvas.height = 800;
+    
+    context.fillStyle = data.urgent ? "#FFCCCC" : "#FFFFFF"; // Red for urgent, white otherwise
     context.fillRect(0, 0, canvas.width, canvas.height);
-
+    
     context.fillStyle = "#000000";
-    context.font = "20px Arial";
-
+    
+    let yPosition = 80; // Adjust based on content
+    let lineHeight = 30;
+    let sectionSepartorLineHeightMultiplier = 2.5
+    let powerTableLineHeightMultiplier = 2
+    
+    
+    if (data.urgent) {
+      context.font = "60px Arial";
+      context.fillText("Urgent!!!", 300, yPosition);
+      yPosition = yPosition + sectionSepartorLineHeightMultiplier * lineHeight;
+    }
+    
+    context.font = "30px Arial";
+    
     // Table Header
-    context.fillText("Specs Power", 50, 50);
-    context.fillText("Spherical", 200, 100);
-    context.fillText("Cylindrical", 400, 100);
-    context.fillText("Axis", 600, 100);
+    context.fillText("Specs Power", 50, yPosition);
+    yPosition = yPosition + sectionSepartorLineHeightMultiplier * lineHeight;
+
+    // Column Headers
+    context.fillText("Spherical", 200, yPosition);
+    context.fillText("Cylindrical", 400, yPosition);
+    context.fillText("Axis", 600, yPosition);
+    yPosition = yPosition + sectionSepartorLineHeightMultiplier * lineHeight;
+
 
     // Helper to draw cells with optional red background
     const drawCell = (text, x, y, isError = false) => {
+      context.strokeStyle = "#000000"; // Black border
       if (isError) {
         context.fillStyle = "#FFCCCC"; // Red background for error
         context.fillRect(x - 10, y - 20, 120, 30);
@@ -82,24 +117,71 @@ function SpecsInput() {
       context.fillText(text || "-", x, y);
     };
 
-    console.log("tor - ", typeof(data.rightCylindrical))
-
     // Right Eye Row
-    context.fillText("Right", 50, 150);
-    drawCell(formatSpecsPower(data.rightSpherical), 200, 150);
-    drawCell(formatSpecsPower(data.rightCylindrical), 400, 150);
-    drawCell(data.rightAxis, 600, 150, data.rightCylindrical && !data.rightAxis);
+    context.fillText("Right", 50, yPosition);
+    drawCell(formatSpecsPower(data.rightSpherical), 200, yPosition);
+    drawCell(formatSpecsPower(data.rightCylindrical), 400, yPosition);
+    drawCell(data.rightAxis, 600, yPosition, data.rightCylindrical && !data.rightAxis);
+    yPosition = yPosition + powerTableLineHeightMultiplier * lineHeight;
 
     // Left Eye Row
-    context.fillText("Left", 50, 200);
-    drawCell(formatSpecsPower(data.leftSpherical), 200, 200);
-    drawCell(formatSpecsPower(data.leftCylindrical), 400, 200);
-    drawCell(data.leftAxis, 600, 200, data.leftCylindrical && !data.leftAxis);
+    context.fillText("Left", 50, yPosition);
+    drawCell(formatSpecsPower(data.leftSpherical), 200, yPosition);
+    drawCell(formatSpecsPower(data.leftCylindrical), 400, yPosition);
+    drawCell(data.leftAxis, 600, yPosition, data.leftCylindrical && !data.leftAxis);
+    yPosition = yPosition + powerTableLineHeightMultiplier * lineHeight;
+
+    if (data.rightAddition || data.leftAddition) {
+      context.fillText("Addition", 50, yPosition);
+      if (data.rightAddition !== data.leftAddition) {
+        context.fillText(formatSpecsPower(data.rightAddition) + " (R)" || "-", 200, yPosition);
+        context.fillText(formatSpecsPower(data.leftAddition) + " (L)" || "-", 400, yPosition);
+      } else {
+        context.fillText(formatSpecsPower(data.rightAddition) + " (Both Eyes)" || "-", 300, yPosition);
+      }
+      yPosition = yPosition + sectionSepartorLineHeightMultiplier * lineHeight;
+    }
+    
+    yPosition = yPosition + lineHeight/1.5
+    lineHeight = lineHeight * 1.5
+
+    const drawLine = (yPosition) => {
+      // Define a new path
+      context.beginPath();
+
+      // Set a start-point
+      context.moveTo(0, yPosition);
+
+      // Set an end-point
+      context.lineTo(800, yPosition);
+
+      // Stroke it (Do the Drawing)
+      context.stroke();
+    }
+
+
 
     // Additional Fields
-    context.fillText(`Lens Description: ${data.lensDescription}`, 50, 300);
-    context.fillText(`Supplier Name: ${data.supplierName}`, 50, 350);
-    context.fillText(`Urgent: ${data.urgent ? "Yes" : "No"}`, 50, 400);
+    if (data.lensDescription) {
+      drawLine(yPosition - lineHeight/1.2);
+      let dividedDesc = divideDescription(data.lensDescription, 50)
+      context.font = "bold 27px Arial";
+      dividedDesc.forEach(descChunk => {
+        context.fillText(`${descChunk}`, 50, yPosition);
+        yPosition = yPosition + lineHeight;
+      })
+      drawLine(yPosition - lineHeight/1.5);
+      yPosition = yPosition + lineHeight/1.5
+    }
+    context.font = "27px Arial";
+    if (data.customerName) {
+      context.fillText(`Customer Name: ${data.customerName}`, 50, yPosition);
+    }
+    yPosition = yPosition + lineHeight;
+    if (data.supplierName) {
+      context.fillText(`Supplier Name: ${data.supplierName}`, 50, yPosition);
+      yPosition = yPosition + lineHeight;
+    }
 
     // Convert canvas to Blob (image)
     canvas.toBlob((blob) => {
@@ -107,7 +189,20 @@ function SpecsInput() {
       setImageUrl(url); // Set image URL for display
       setImageBlob(blob); // Store Blob for sharing
     });
+
+    
+    setTimeout(window.scrollTo(0, document.body.scrollHeight), 100);
   };
+
+  const handleCopyAddition = () => {
+    if (leftAddition === "" && rightAddition !== "") {
+      setLeftAddition(rightAddition);
+    } else if (rightAddition === "" && leftAddition !== "") {
+      setRightAddition(leftAddition);
+    } else {
+      setLeftAddition(rightAddition);
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -122,6 +217,7 @@ function SpecsInput() {
     }
 
     generateImage({
+      customerName,
       rightSpherical: rightSph,
       rightCylindrical: rightCyl,
       rightAxis: rightAxis,
@@ -131,8 +227,35 @@ function SpecsInput() {
       lensDescription: lensDescription,
       supplierName: supplierName,
       urgent: urgent,
+      rightAddition: rightAddition,
+      leftAddition: leftAddition,
     });
   };
+
+  const handleReset = () => {
+    setRightSph("");
+    setRightCyl("");
+    setRightAxis("");
+    setLeftSph("");
+    setLeftCyl("");
+    setLeftAxis("");
+    setLensDescription("");
+    setSupplierName("");
+    setUrgent(false);
+    setImageUrl(null);
+    setImageBlob(null);
+  };
+
+  const rightClick = (e) => {
+    const rightClickEvent = new MouseEvent('contextmenu', {
+      bubbles: true,
+      cancelable: true,
+      button: 2 // Right-click button
+    });
+    
+    // Dispatch the event on the target element
+    e.target.dispatchEvent(rightClickEvent);
+  }
 
   return (
     <div className="container">
@@ -141,18 +264,18 @@ function SpecsInput() {
         {/* Power Table */}
         <div className="power-table">
           <div className="row">
-            <span>Right</span>
+            <span className="power-input-lr-span">R</span>
             <input
               type="number"
               value={rightSph}
               onChange={(e) => setRightSph(e.target.value)}
-              placeholder="Spherical Power"
+              placeholder="Sph"
             />
             <input
               type="number"
               value={rightCyl}
               onChange={(e) => setRightCyl(e.target.value)}
-              placeholder="Cylindrical Power"
+              placeholder="Cyl"
             />
             <input
               type="number"
@@ -162,18 +285,18 @@ function SpecsInput() {
             />
           </div>
           <div className="row">
-            <span>Left</span>
+            <span className="power-input-lr-span">L</span>
             <input
               type="number"
               value={leftSph}
               onChange={(e) => setLeftSph(e.target.value)}
-              placeholder="Spherical Power"
+              placeholder="Sph"
             />
             <input
               type="number"
               value={leftCyl}
               onChange={(e) => setLeftCyl(e.target.value)}
-              placeholder="Cylindrical Power"
+              placeholder="Cyl"
             />
             <input
               type="number"
@@ -184,17 +307,54 @@ function SpecsInput() {
           </div>
         </div>
 
+        {/* Addition Table */}
+        <div className="row">
+          <span className="power-input-lr-span">Addition</span>
+          <input
+            type="number"
+            value={rightAddition}
+            onChange={(e) => setRightAddition(e.target.value)}
+            placeholder="Right Add."
+          />
+          <input
+            type="number"
+            value={leftAddition}
+            onChange={(e) => setLeftAddition(e.target.value)}
+            placeholder="Left Add."
+          />
+        </div>
+
+        <div id='copy-btn-container'>
+          <button id='copy-addition-btn' type="button" onClick={handleCopyAddition}>Copy Addition</button>
+        </div>
+
+        <hr></hr>
+
         {/* Lens Description */}
-        <div className="input-group">
+        <div id="lens-desc-container" className="input-group">
           <label>
             Lens Description:
             <textarea
               value={lensDescription}
               onChange={(e) => setLensDescription(e.target.value)}
               placeholder="Enter lens description"
+              maxLength={142}
             />
           </label>
         </div>
+
+      {/* Supplier Name */}
+      <div className="input-group">
+        <label>
+          Customer Name:
+          <input
+            type="text"
+            value={customerName}
+            onChange={(e) => setCustomerName(e.target.value)}
+            placeholder="Enter customer name"
+          />
+        </label>
+      </div>
 
         {/* Supplier Name */}
         <div className="input-group">
@@ -210,26 +370,29 @@ function SpecsInput() {
         </div>
 
         {/* Urgent Checkbox */}
-        <div className="input-group">
-          <label>
+        <div className="input-group-special">
+          <label className="custom-checkbox">
             Urgent:
             <input
               type="checkbox"
               checked={urgent}
               onChange={(e) => setUrgent(e.target.checked)}
             />
+            <span className="checkmark"></span>
           </label>
         </div>
 
+
         {/* Submit Button */}
         <button type="submit">Generate Image</button>
+        <button type="button" onClick={handleReset}>Reset</button>
       </form>
 
       {/* Share Button */}
       {imageUrl && (
         <div className="share-section">
           {/* <button onClick={handleShare}>Share on WhatsApp</button> */}
-          <img src={imageUrl} alt="Generated Specs Image" className="image-preview" />
+          <img id="img" src={imageUrl} alt="Generated Specs Image" className="image-preview" onClick={rightClick} />
         </div>
       )}
     </div>
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsxcQpru6PRkgzfW9CWyjHz6VJztdd-Ms",
+    authDomain: "specs-order.firebaseapp.com",
+    projectId: "specs-order",
+    storageBucket: "specs-order.firebasestorage.app",
+    messagingSenderId: "742277008785",
+    appId: "1:742277008785:web:93ff69f90e73a8e6991e30",
+    measurementId: "G-QXSVZS2LVH"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { auth, provider, signInWithPopup, db, analytics };
